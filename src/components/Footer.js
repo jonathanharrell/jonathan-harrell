5de0d4703a376f88b83b8f95f8deff228546bf54
Jonathan Harrell <harr041@gmail.com>
2019-10-17 21:04:43 -0500

    wip

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,92 +1,55 @@
 import React from 'react'
-import { Link } from 'gatsby'
+import styled from 'styled-components'
+import { Link as GatsbyLink } from 'gatsby'
+import Padded from '../jh-ui/Padded'
+import { breakpoints } from '../jh-ui/themes'
+import ContentWrapper from './ContentWrapper'
 
-import logo from '../img/logo.svg'
-import facebook from '../img/social/facebook.svg'
-import instagram from '../img/social/instagram.svg'
-import twitter from '../img/social/twitter.svg'
-import vimeo from '../img/social/vimeo.svg'
+const FooterWrap = styled.footer`
+  background-color: ${({ theme }) => theme.colors.backgroundInverse};
+  color: ${({ theme }) => theme.colors.textLighter};
+`
+
+const FooterContentWrapper = styled(ContentWrapper)`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`
+
+const Link = styled(GatsbyLink)`
+  font-size: ${({ theme }) => theme.fontSizes.s.mobile};
+  text-decoration: none;
+  color: ${({ theme }) => theme.colors.textLighter};
+  
+  @media (min-width: ${breakpoints.tablet}) {
+    font-size: ${({ theme }) => theme.fontSizes.s.tablet};
+  }
+  
+  @media (min-width: ${breakpoints.desktop}) {
+    font-size: ${({ theme }) => theme.fontSizes.s.desktop};
+  }
+`
 
 const Footer = class extends React.Component {
   render() {
     return (
-      <footer className="footer has-background-black has-text-white-ter">
-        <div className="content has-text-centered">
-          <img
-            src={logo}
-            alt="Kaldi"
-            style={{ width: '14em', height: '10em' }}
-          />
-        </div>
-        <div className="content has-text-centered has-background-black has-text-white-ter">
-          <div className="container has-background-black has-text-white-ter">
-            <div className="columns">
-              <div className="column is-4">
-                <section className="menu">
-                  <ul className="menu-list">
-                    <li>
-                      <Link to="/" className="navbar-item">
-                        Home
-                      </Link>
-                    </li>
-                    <li>
-                      <Link className="navbar-item" to="/about">
-                        About
-                      </Link>
-                    </li>
-                    <li>
-                      <Link className="navbar-item" to="/blog">
-                        Latest Stories
-                      </Link>
-                    </li>
-                    <li>
-                      <a
-                        className="navbar-item"
-                        href="/admin/"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                      >
-                        Admin
-                      </a>
-                    </li>
-                  </ul>
-                </section>
-              </div>
-              <div className="column is-4 social">
-                <a title="facebook" href="https://facebook.com">
-                  <img
-                    src={facebook}
-                    alt="Facebook"
-                    style={{ width: '1em', height: '1em' }}
-                  />
-                </a>
-                <a title="twitter" href="https://twitter.com">
-                  <img
-                    className="fas fa-lg"
-                    src={twitter}
-                    alt="Twitter"
-                    style={{ width: '1em', height: '1em' }}
-                  />
-                </a>
-                <a title="instagram" href="https://instagram.com">
-                  <img
-                    src={instagram}
-                    alt="Instagram"
-                    style={{ width: '1em', height: '1em' }}
-                  />
-                </a>
-                <a title="vimeo" href="https://vimeo.com">
-                  <img
-                    src={vimeo}
-                    alt="Vimeo"
-                    style={{ width: '1em', height: '1em' }}
-                  />
-                </a>
-              </div>
+      <FooterWrap>
+        <Padded vertical="l">
+          <FooterContentWrapper>
+            <Link to="/">
+              &copy; 2017–2019 Jonathan Harrell
+            </Link>
+            <div>
+              <a href="">tw</a>
+              <a href="">gh</a>
+              <a href="">co</a>
+              <a href="">li</a>
+              <a href="">in</a>
+              <a href="">rss</a>
             </div>
-          </div>
-        </div>
-      </footer>
+          </FooterContentWrapper>
+        </Padded>
+      </FooterWrap>
     )
   }
 }
