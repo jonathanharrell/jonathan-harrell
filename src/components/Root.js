cb5fcdf3de0367ef0b3b8471be3805d6d060c5f2
Jonathan Harrell <harr041@gmail.com>
2019-10-28 21:27:58 -0500

    wip

--- src/components/Root.js
+++ src/components/Root.js
@@ -1,45 +1,44 @@
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
 import themes from '../jh-ui/themes'
 import ThemeContext from '../context/theme'
 
 const Root = ({ children }) => {
-  const [theme, setTheme] = useState(themes.light)
+  let defaultTheme = themes.light
 
-  useEffect(() => {
-    const darkModeMediaQuery = window.matchMedia('(prefers-color-scheme: dark)')
+  const darkModeMediaQuery = typeof window !== 'undefined' && window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)')
 
-    // if system has dark theme, set theme to dark
-    if (darkModeMediaQuery.matches) {
-      setTheme(themes.dark)
-    }
+  // if system has dark theme, set theme to dark
+  if (darkModeMediaQuery && darkModeMediaQuery.matches) {
+    defaultTheme = themes.dark
+  }
 
-    // look if there is a saved theme
-    const savedTheme = sessionStorage.getItem('jh-theme')
+  // look if there is a saved theme
+  const savedTheme = typeof sessionStorage !== 'undefined' && sessionStorage.getItem('jh-theme')
 
-    // if saved theme, set theme to that
-    // otherwise, save the current theme
-    if (savedTheme) {
-      setTheme(themes[savedTheme])
-    } else if (sessionStorage) {
-      sessionStorage.setItem('jh-theme', theme === themes.light ? 'light' : 'dark')
-    }
+  // if saved theme, set theme to that
+  // otherwise, save the current theme
+  if (savedTheme) {
+    defaultTheme = themes[savedTheme]
+  } else if (typeof sessionStorage !== 'undefined') {
+    sessionStorage.setItem('jh-theme', defaultTheme === themes.light ? 'light' : 'dark')
+  }
 
-    darkModeMediaQuery.addListener(() => {
-      // set and store newly changed system theme
-      const newThemeName = darkModeMediaQuery.matches ? 'dark' : 'light'
-      setTheme(themes[newThemeName])
-      sessionStorage.setItem('jh-theme', newThemeName)
-    })
-  }, [theme])
+  const [theme, setTheme] = useState(defaultTheme)
 
   const toggleTheme = () => {
     // set and store newly toggled theme
     const newThemeName = theme === themes.light ? 'dark' : 'light'
     setTheme(themes[newThemeName])
+    if (typeof sessionStorage !== 'undefined') sessionStorage.setItem('jh-theme', newThemeName)
+  }
 
-    if (typeof sessionStorage !== 'undefined') {
-      sessionStorage.setItem('jh-theme', newThemeName)
-    }
+  if (darkModeMediaQuery) {
+    darkModeMediaQuery.addListener(() => {
+      // set and store newly changed system theme
+      const newThemeName = darkModeMediaQuery.matches ? 'dark' : 'light'
+      setTheme(themes[newThemeName])
+      if (typeof sessionStorage !== 'undefined') sessionStorage.setItem('jh-theme', newThemeName)
+    })
   }
 
   return (
