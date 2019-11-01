700ada8b5605a76b58b5fd48c7ab71b98b800f6c
Jonathan Harrell <harr041@gmail.com>
2019-10-31 21:07:28 -0500

    wip

--- src/cms/preview-templates/BlogPostPreview.js
+++ src/cms/preview-templates/BlogPostPreview.js
@@ -1,15 +1,21 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { BlogPostTemplate } from '../../templates/blog-post'
+import { ThemeProvider } from 'styled-components'
+import theme from '../../jh-ui/theme'
+import ThemeContext from '../../context/theme'
 
-const BlogPostPreview = ({ entry, widgetFor }) => (
-  <BlogPostTemplate
-    content={widgetFor('body')}
-    description={entry.getIn(['data', 'description'])}
-    tags={entry.getIn(['data', 'tags'])}
-    title={entry.getIn(['data', 'title'])}
-  />
-)
+const BlogPostPreview = ({ entry, widgetFor }) => {
+  return (
+    <ThemeContext.Provider value={{ theme }}>
+      <div style={{ fontFamily: `'Roboto', 'Helvetica Neue', 'Helvetica', 'Arial', 'sans-serif'` }}>
+        <ThemeProvider theme={theme}>
+          <h1>{entry.getIn(['data', 'title'])}</h1>
+          {widgetFor('body')}
+        </ThemeProvider>
+      </div>
+    </ThemeContext.Provider>
+  )
+}
 
 BlogPostPreview.propTypes = {
   entry: PropTypes.shape({
