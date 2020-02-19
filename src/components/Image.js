1000167293eb029299e9ac812cebba8437f60b93
Jonathan Harrell <harr041@gmail.com>
2020-02-18 19:35:39 -0600

    wip

--- src/components/Image.js
+++ src/components/Image.js
@@ -1,5 +1,13 @@
 import React, { useEffect, useMemo, useState } from 'react'
+import styled from 'styled-components'
 import Text from '../jh-ui/Text'
+
+const SvgWrap = styled.div`
+  svg {
+    width: 100%;
+    height: auto;
+  }
+`
 
 const Image = ({ src, alt, title }) => {
   const [markup, setMarkup] = useState('')
@@ -39,12 +47,14 @@ const Image = ({ src, alt, title }) => {
       .replace(/#D4D4DD/g, 'var(--illustrationLineShade2)')
       .replace(/#C9C9D6/g, 'var(--illustrationLineShade3)')
       .replace(/#C1C1D2/g, 'var(--illustrationLineShade3)')
+      .replace(/#F9F9FB/g, 'var(--backgroundSecondary)')
+      .replace(/#2E3138/g, 'var(--text)')
   }, [markup])
 
   return (
     <>
       {processedMarkup && (
-        <div dangerouslySetInnerHTML={{ __html: processedMarkup }}/>
+        <SvgWrap dangerouslySetInnerHTML={{ __html: processedMarkup }}/>
       )}
       <noscript>
         <img src={src} alt={alt}/>
