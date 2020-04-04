fd4b02c863c6d52b83929db2427d080b54706bbf
Jonathan Harrell <harr041@gmail.com>
2020-04-04 18:29:40 -0500

    favicons

--- website-config.js
+++ website-config.js
@@ -1,21 +1,24 @@
 module.exports = {
   pathPrefix: '/',
-  title: 'Jonathan Harrell | CSS Blogger & Teacher, UI/UX Designer, Front-End Developer', // Navigation and Site Title
+  title:
+    'Jonathan Harrell | CSS Blogger & Teacher, UI/UX Designer, Front-End Developer', // Navigation and Site Title
   titleAlt: 'Jonathan Harrell', // Title for JSONLD
-  description: 'Want to stay up-to-date on the latest developments in CSS and JavaScript? Get tips, tutorials and thoughts from designer/developer Jonathan Harrell.',
-  headline: 'Get tips, tutorials and thoughts from designer/developer Jonathan Harrell.', // Headline for schema.org JSONLD
+  description:
+    'Want to stay up-to-date on the latest developments in CSS and JavaScript? Get tips, tutorials and thoughts from designer/developer Jonathan Harrell.',
+  headline:
+    'Get tips, tutorials and thoughts from designer/developer Jonathan Harrell.', // Headline for schema.org JSONLD
   url: 'https://jonathan-harrell.netlify.com', // Domain of your site. No trailing slash!
   siteLanguage: 'en', // Language Tag on <html> element
-  logo: '/img/favicon-32x32.png', // Used for SEO
+  logo: '/img/favicon-180x180.png', // Used for SEO
   ogLanguage: 'en_US', // Facebook Language
 
   // JSONLD / Manifest
-  favicon: 'static/img/favicon-32x32.png', // Used for manifest favicon generation
+  favicon: 'src/favicon.png', // Used for manifest favicon generation
   shortName: 'J. Harrell', // shortname for manifest. MUST be shorter than 12 characters
   author: 'Jonathan Harrell', // Author for schemaORGJSONLD
   themeColor: '#ffffff',
   backgroundColor: '#ffffff',
 
   twitterUsername: '@HarrellofDurham', // Twitter Username
-  facebookAppId: '285210128750934', // Facebook App Id
+  facebookAppId: '285210128750934' // Facebook App Id
 }
