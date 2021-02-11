7d3d7595946e31b243618b61251be83cf8881063
Jonathan Harrell <harr041@gmail.com>
2021-02-11 11:37:40 -0600

    wip

--- src/components/Codepen.js
+++ src/components/Codepen.js
@@ -1,24 +1,32 @@
 import React, { useContext, useEffect, useRef } from "react";
 import ReactDOMServer from "react-dom/server";
-import PropTypes from "prop-types";
 import ThemeContext from "../context/theme";
 
-const Codepen = ({ id, children }) => {
+const Codepen = ({ children }) => {
 	const { themeName } = useContext(ThemeContext);
 	const root = useRef();
+
+	let codepenChildren = null;
+	if (children) codepenChildren = children.length ? children : [children];
 
 	useEffect(() => {
 		window.__CPEmbed(root.current.querySelector(".codepen"));
 	}, [themeName]);
 
 	const renderForm = () => {
-		const data = children.reduce((obj, child) => {
-			obj[child.props["data-lang"]] = ReactDOMServer.renderToString(child.props.children);
+		const data = codepenChildren.reduce((obj, child) => {
+			obj[child.props["data-lang"]] =
+				child.props["data-lang"] === "html"
+					? ReactDOMServer.renderToString(child.props.children)
+					: child.props.children;
 			return obj;
 		}, {});
 
 		data.html_classes = [themeName];
-		data.stylesheets = "https://cdn.jsdelivr.net/npm/hiq@4.1.4/dist/hiq.min.css";
+		data.stylesheets = [
+			"https://cdn.jsdelivr.net/npm/hiq@4.1.4/dist/hiq.min.css",
+			"https://60255a16a1fb6c0007fe1c96--jonathan-harrell.netlify.app/codepen.css"
+		];
 
 		return (
 			<form action="https://codepen.io/pen/define" method="POST" target="_blank">
@@ -31,7 +39,7 @@ const Codepen = ({ id, children }) => {
 		root.current.querySelector("form").submit();
 	};
 
-	return children ? (
+	return codepenChildren ? (
 		<>
 			<div ref={root} className="my-8 border-4 border-gray-50 dark:border-gray-800">
 				<div>
@@ -43,13 +51,16 @@ const Codepen = ({ id, children }) => {
 					className="codepen hidden"
 					data-prefill={`{
 						"html_classes": ["${themeName}"],
-						"stylesheets": "https://cdn.jsdelivr.net/npm/hiq@4.1.4/dist/hiq.min.css"
+						"stylesheets": [
+							"https://cdn.jsdelivr.net/npm/hiq@4.1.4/dist/hiq.min.css",
+							"https://60255a16a1fb6c0007fe1c96--jonathan-harrell.netlify.app/codepen.css"
+						]
 					}`}
 					data-height="180"
 					data-theme-id="39442"
 					data-default-tab="result"
 				>
-					{children.map((child, index) => (
+					{codepenChildren.map((child, index) => (
 						<pre key={index} data-lang={child.props["data-lang"]}>
 							{child.props["data-lang"] === "html"
 								? ReactDOMServer.renderToString(child.props.children)
@@ -60,10 +71,6 @@ const Codepen = ({ id, children }) => {
 			</div>
 		</>
 	) : null;
-};
-
-Codepen.propTypes = {
-	id: PropTypes.string.isRequired
 };
 
 export default Codepen;
