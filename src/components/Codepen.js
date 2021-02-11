8fa7349bb204360b4e68f239f59f24ad030aaafd
Jonathan Harrell <harr041@gmail.com>
2021-02-11 10:00:42 -0600

    wip

--- src/components/Codepen.js
+++ src/components/Codepen.js
@@ -11,15 +11,40 @@ const Codepen = ({ id, children }) => {
 		window.__CPEmbed(root.current.querySelector(".codepen"));
 	}, [themeName]);
 
+	const renderForm = () => {
+		const data = children.reduce((obj, child) => {
+			obj[child.props["data-lang"]] = ReactDOMServer.renderToString(child.props.children);
+			return obj;
+		}, {});
+
+		data.html_classes = [themeName];
+		data.stylesheets = "https://cdn.jsdelivr.net/npm/hiq@4.1.4/dist/hiq.min.css";
+
+		return (
+			<form action="https://codepen.io/pen/define" method="POST" target="_blank">
+				<input type="hidden" name="data" value={JSON.stringify(data)} />
+			</form>
+		);
+	};
+
+	const viewOnCodepen = () => {
+		root.current.querySelector("form").submit();
+	};
+
 	return (
 		<>
 			<div ref={root} className="my-8 border-4 border-gray-50 dark:border-gray-800">
+				<div>
+					<span>Try it out!</span>
+					<button onClick={viewOnCodepen}>View it on Codepen</button>
+					<div className="hidden">{renderForm()}</div>
+				</div>
 				<div
 					className="codepen hidden"
 					data-prefill={`{
-					"html_classes": ["${themeName}"],
-					"stylesheets": "https://cdn.jsdelivr.net/npm/hiq@4.1.4/dist/hiq.min.css"
-				}`}
+						"html_classes": ["${themeName}"],
+						"stylesheets": "https://cdn.jsdelivr.net/npm/hiq@4.1.4/dist/hiq.min.css"
+					}`}
 					data-height="180"
 					data-theme-id="39442"
 					data-default-tab="result"
